ae7c6816a1b4f23b8ac160a597446fee02840ff6
leothesealion <leothesealion@gmail.com>
2024-11-05 22:36:07 -0600

    Updated server.js to include the chat.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const http = require('http');
 const socketIo = require('socket.io');
 const authRoutes = require('./routes/auth');
+const chatRoutes = require('./routes/chat');
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/chat', chatRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
@@ -36,6 +38,7 @@ io.on('connection', (socket) => {
     console.log('Client disconnected');
   });
 });
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
