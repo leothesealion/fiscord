c326a74527bf9f6f06c2a3318d9dda9a96b0e2b0
leothesealion <leothesealion@gmail.com>
2024-11-05 22:51:11 -0600

    Updated authController.js to support user profile fetching

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -79,3 +79,14 @@ exports.login = async (req, res) => {
     res.status(500).send('Server error');
   }
 };
+
+// Get authenticated user's profile
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
