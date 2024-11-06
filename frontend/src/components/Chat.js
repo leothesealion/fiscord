daec1e8ef2374c7f1660323f08182fe5f88b2915
leothesealion <leothesealion@gmail.com>
2024-11-05 22:47:09 -0600

    Updated chat.js

---
+++ frontend/src/components/Chat.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000');
+
+const Chat = () => {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on('receiveMessage', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.off('receiveMessage');
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('sendMessage', message);
+    setMessage('');
+  };
+
+  return (
+    <div>
+      <h1>Chat</h1>
+      <div>
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+};
+
+export default Chat;
